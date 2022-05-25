87aa952a2c165018c8da386c83c69186d8bcb17f
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-05-25 18:23:01 +0100

    master

--- Temp-Files/src/template/HomeDemo3/SecFAQ_Timeline/SingleFAQArea/index.jsx
+++ Temp-Files/src/template/HomeDemo3/SecFAQ_Timeline/SingleFAQArea/index.jsx
@@ -1,14 +1,63 @@
+import { useEffect, useState} from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { injested } from './injested';
+import swal from 'sweetalert';
 
-const SingleFAQArea = ({text , ID}) => {
+
+function SingleFAQArea () {
 
   return (
-  	<>
-	    <dt className="wave" data-toggle="collapse" data-target={`#${ID}`}>{text}</dt>
-	    <dd data-aos="fade-up" id={ID} className="collapse">
-	        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore omnis quaerat nostrum, pariatur ipsam sunt accusamus enim necessitatibus est fugiat, assumenda dolorem, deleniti corrupti cupiditate ipsum, dolorum voluptatum esse error?</p>
-	    </dd>
+  	
+	  <div className="ico-counter">
+	     <div className="counter-down">
 
-  	</>
+		<div className="content">
+		
+			<div className="text-center">
+				{
+					active ?
+						
+						<a  className="button mt-30" > {balance} MTV {`${account.substring(0 , 5)}........${account.substring(38)}`}</a>
+						:
+						<a  className="button mt-30" onClick={connect}>connect </a>
+						
+						
+						
+						
+
+				}
+
+				<a  className="button mt-31" onClick={disconnect}>disconnect</a>
+			    
+				<h3 className="text-center mb-30">TOKEN SALE ENDS IN</h3>
+			</div>
+			
+			<div className="counterdown-content">
+			
+				<div className="count-down titled circled text-center">
+				
+				</div>
+				<div className="ico-progress">
+				<ul className="list-unstyled list-percent list-inline clearfix mb-10">
+					<li className="title">33m</li>
+					<li className="strength">75m</li>
+				</ul>
+				<div className="current-progress">
+			
+				</div>
+				
+				</div>
+				<div className="text-center">
+					<a href="#" className="button mt-30">Buy More Tokens</a>
+					<a></a>
+					<a href="#" className="button mt-30">Claim Token</a>
+				</div>
+			</div>
+			
+		</div>
+
+</div>
+</div>
   );
 }
 
