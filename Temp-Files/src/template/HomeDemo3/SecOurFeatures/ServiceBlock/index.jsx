b91aadbd376940c707625f002c64fa42f2d75578
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-05-27 17:09:55 +0100

    master

--- Temp-Files/src/template/HomeDemo3/SecOurFeatures/ServiceBlock/index.jsx
+++ Temp-Files/src/template/HomeDemo3/SecOurFeatures/ServiceBlock/index.jsx
@@ -1,19 +1,7 @@
-import ab from "../../../../asset/images/azc1.png";
-import ac from "../../../../asset/images/daily.png";
-import ad from "../../../../asset/images/digi.png";
 
-const featuresData = [
-  {
-    ng : ab
-  },
-  {
-    ng : ac
-  },
-  {
-    ng : ad
-  },
+
   
-];
+
 const ServiceBlock = ({classIco , title, ng}) => {
 
   return (
@@ -23,10 +11,7 @@ const ServiceBlock = ({classIco , title, ng}) => {
                 <span className={classIco}></span>
             </div>
             <h3><a href="#">{title}</a></h3>
-            {featuresData.map((item, index) => (
-          <Feature title={item.title} text={item.text} key={item.title + index} />
-          ))}
-            <div className="text">{ng}</div>
+            
         </div>
     </div>
   );
