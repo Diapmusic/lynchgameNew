782e0598a7148a9af49c1db8a242f9694a98779d
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-05-27 23:18:28 +0100

    master

--- Temp-Files/src/App.jsx
+++ Temp-Files/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     	<div className="App">
         <Helmet>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>
             LYNCH
           </title>
